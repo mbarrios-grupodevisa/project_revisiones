0e64f097a86d525a4cc8726b6e50f757034ae450
Enrique Matul <ematul@grupodevisa.com>
2016-07-11 16:25:48 -0600

    Solución de problema Elemento

--- app/src/main/java/gt/com/metrocasas/revisiones/ElementoAdapter.java
+++ app/src/main/java/gt/com/metrocasas/revisiones/ElementoAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,8 +27,8 @@ import static gt.com.metrocasas.revisiones.R.style.boton;
 public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyViewHolder> {
 
     private List<Elemento> listElemento;
-    private Elemento object;
     private Activity actividad;
+    private int tam = 0;
 
     public ElementoAdapter(List<Elemento> listElemento,Activity ac) {
         this.listElemento = listElemento;
@@ -38,13 +39,17 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyView
         public CheckBox estado;
         public TextView elemento;
         public Button agregar_comentario;
+        public Elemento e;
 
-        public MyViewHolder(View view) {
+        public MyViewHolder(View view,Elemento it) {
             super(view);
 
             estado = (CheckBox) view.findViewById(R.id.element_checkBox);
             elemento = (TextView) view.findViewById(R.id.element_revision);
             agregar_comentario = (Button) view.findViewById(R.id.element_btn);
+            this.e  = it;
+
+
 
             estado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
@@ -52,11 +57,11 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyView
                     if(isChecked){
                         agregar_comentario.setEnabled(false);
                         elemento.setPaintFlags(elemento.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                        object.setEstado(true);
+                        e.setEstado(true);
                     }else{
                         elemento.setPaintFlags(elemento.getPaintFlags() &~Paint.STRIKE_THRU_TEXT_FLAG);
                         agregar_comentario.setEnabled(true);
-                        object.setEstado(false);
+                        e.setEstado(false);
                     }
                 }
             });
@@ -65,7 +70,7 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyView
                 @Override
                 public void onClick(View view) {
                     comentarioDialog comentario = new comentarioDialog();
-                    comentario.setElement(object);
+                    comentario.setElement(e);
                     comentario.setBtn_coment(agregar_comentario);
                     comentario.show(actividad.getFragmentManager(),"");
                 }
@@ -78,14 +83,16 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyView
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_elemento, parent, false);
-        return new MyViewHolder(itemView);
+        Elemento item = listElemento.get(tam);
+        tam++;
+        Log.i("Tam:",tam+"");
+        return new MyViewHolder(itemView,item);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         Elemento item = listElemento.get(position);
         holder.elemento.setText(item.getElemento());
-        object = item;
     }
 
     @Override
@@ -126,6 +133,8 @@ public class ElementoAdapter extends RecyclerView.Adapter<ElementoAdapter.MyView
         CardView card_view = (CardView) actividad.findViewById(R.id.card_view_construccion);
         card_view.setVisibility(View.GONE);
     }
+
+
 
 
 
